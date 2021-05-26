aeb27301c8b65e25c3e8d8042001cdcfd5175645
tuan <tuanphung2308@gmail.com>
2021-05-27 00:36:11 +0700

    Noti for wang

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import requests
 import json
 from beepy import beep
 import time
+from win10toast import ToastNotifier
+
 wallet_address = '9fuoqNLuFea3ghYQsR9Hv1NU9AWsLjDrKU4fC1CKKQKLhJLmvyj'
 worker_uri = f'https://api.nanopool.org/v1/ergo/workers/{wallet_address}'
 
 while True:
+    toast = ToastNotifier()
     worker_response = requests.get(worker_uri)
     parsed_workers = json.loads(worker_response.text)
     workers_data = parsed_workers['data']
@@ -13,8 +16,8 @@ while True:
     for worker_data in workers_data:
         if worker_data['hashrate'] == 0:
             for i in range(1000):
+                toast.show_toast("Notification", "WANGGGGGGGGGGGGGGG RIG DOWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
+                                 duration=20)
                 beep(sound="ping")
                 time.sleep(1)
     time.sleep(30)
-
-
